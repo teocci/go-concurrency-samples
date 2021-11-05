4dd5f8c12aa65e3a3c828a8d373736f52813c467
RTT PC <teocci@yandex.com>
2021-11-06 01:55:37 +0900

    Add merge, extract and run scripts

---
+++ run-extract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## Run proclogs and extract merged data
+./proclogs -f ./tmp/merged-01.data-new.zip -e
---
+++ run-merge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## Run proclogs and merge and extract split data into a tmp dir
+./proclogs -f ./split-big-files/01.data-new.zip.pt00 -m
